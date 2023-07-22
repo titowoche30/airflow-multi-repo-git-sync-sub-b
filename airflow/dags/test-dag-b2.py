0b719123b4e6878f7dd40bf3497f60fc7888f833
titowoche30 <titowoche30@gmail.com>
2023-07-22 16:41:47 -0300

    adding example dags

---
+++ airflow/dags/test-dag-b2.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
+default_args = {
+    "depends_on_past": False,
+    "start_date": datetime(2023, 7, 22),
+    "retries": 0
+}
+
+with DAG(
+    dag_id="test-dag-b2",
+    schedule_interval="@once",
+    default_args=default_args,
+    catchup=False,
+    max_active_runs=1,
+    tags=["sync-sub-b"],
+) as dag:
+
+    test_task = BashOperator(
+        task_id="test_task",
+        bash_command="echo 'Hello K8S Airflow World from DAG B2'",
+        do_xcom_push=False,
+    )
